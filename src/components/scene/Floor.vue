f47955b3f18d3f93f4d1537df86e72486d0f9d1b
Miguel Cid Flor <cidflor2004@gmail.com>
2026-06-01 00:38:57 +0100

    chore: Correct formatting

--- src/components/scene/Floor.vue
+++ src/components/scene/Floor.vue
@@ -6,29 +6,34 @@ import { Box3, Vector3 } from "three";
 const { state: model } = useGLTF("/models/room/planks.glb");
 const plankScale = ref(1);
 const plankWidth = 0.5;
-const floorScale = 8;
+const floorScale = 5;
 const scenes = ref<any[]>([]);
 const positions = ref<[number, number, number][]>([]);
 
 watch(model, (loaded) => {
-    if (!loaded) return;
+  if (!loaded) return;
 
-    const box = new Box3();
-    box.expandByObject(loaded.scene);
-    const plankSize = new Vector3();
-    box.getSize(plankSize);
+  const box = new Box3();
+  box.expandByObject(loaded.scene);
+  const plankSize = new Vector3();
+  box.getSize(plankSize);
+  plankScale.value = plankWidth / plankSize.x;
 
-    plankScale.value = plankWidth / plankSize.x;
-
-    for (let i = -floorScale / 2; i < floorScale / 2; i += plankWidth) {
-        for (let j = -floorScale / 2; j < floorScale / 2; j += plankWidth) {
-            positions.value.push([i, 0, j]);
-            scenes.value.push(markRaw(loaded.scene.clone()));
-        }
+  for (let i = -floorScale / 2; i < floorScale / 2; i += plankWidth) {
+    for (let j = -floorScale / 2; j < floorScale / 2; j += plankWidth) {
+      positions.value.push([i, -plankSize.y * plankScale.value, j]);
+      scenes.value.push(markRaw(loaded.scene.clone()));
     }
+  }
 });
 </script>
 
 <template>
-    <primitive v-for="(scene, i) in scenes" :key="i" :object="scene" :position="positions[i]" :scale="plankScale" />
+  <primitive
+    v-for="(scene, i) in scenes"
+    :key="i"
+    :object="scene"
+    :position="positions[i]"
+    :scale="plankScale"
+  />
 </template>
--- src/components/scene/Room.vue
+++ src/components/scene/Room.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import Floor from "./Floor.vue";
+import SideWall from "./SideWall.vue";
 </script>
 
 <template>
-    <TresPerspectiveCamera :position="[5, 0, 5]" :look-at="[0, 0, 0]" />
+    <TresPerspectiveCamera :position="[6, 1.5, 4]" :look-at="[0, 0, 0]" />
     <TresAmbientLight :intensity="0.5" color="white" />
     <TresGroup>
         <Floor />
+        <SideWall />
     </TresGroup>
     <TresAxesHelper />
     <TresDirectionalLight :position="[0, 2, 4]" :intensity="1" cast-shadow />
