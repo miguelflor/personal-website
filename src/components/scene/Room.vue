43f2b05018d0f24e74ce5aca2252af8deab9c558
Your Name <you@example.com>
2026-06-25 18:32:34 +0100

    fix(light): The light is mutch more realistic

--- src/components/scene/Room.vue
+++ src/components/scene/Room.vue
@@ -1,23 +1,20 @@
 <script setup lang="ts">
-import { ref, onMounted } from "vue";
-import { DirectionalLight } from "three";
+import { onMounted, ref } from "vue";
+import type { SpotLight } from "three";
 import Floor from "./Floor.vue";
 import SideWall from "./SideWall.vue";
 import { Side } from "../../types";
 
 const floorSize = 5;
-const light = ref<DirectionalLight>();
+const light = ref<SpotLight>();
 
 onMounted(() => {
-  if (!light.value) return;
+  if (!light.value) {
+    return;
+  }
   light.value.shadow.mapSize.set(2048, 2048);
-  light.value.shadow.camera.near = 0.1;
+  light.value.shadow.camera.near = 0.5;
   light.value.shadow.camera.far = 20;
-  light.value.shadow.camera.left = -6;
-  light.value.shadow.camera.right = 6;
-  light.value.shadow.camera.top = 6;
-  light.value.shadow.camera.bottom = -6;
-  light.value.shadow.camera.updateProjectionMatrix();
 });
 </script>
 
@@ -31,6 +28,14 @@ onMounted(() => {
     <SideWall :floorSize="floorSize" :side="Side.Right" />
   </TresGroup>
   <TresAxesHelper />
-  <TresDirectionalLight ref="light" :position="[0, 6, -3]" :intensity="1" cast-shadow />
+  <TresSpotLight
+    ref="light"
+    :position="[0, 5, 0]"
+    :intensity="20"
+    :angle="Math.PI / 3"
+    :penumbra="0.5"
+    :decay="2"
+    cast-shadow
+  />
   <TresGridHelper :args="[10, 10]" />
 </template>
