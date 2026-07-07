d490b4e7bcd7a5050c7a1a3d8a99c219de1aeed7
Your Name <cidflor2004@gmail.com>
2026-07-07 11:42:25 +0100

    config: added @ as src/

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import glsl from 'vite-plugin-glsl'
-import { templateCompilerOptions } from '@tresjs/core'
-
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import glsl from "vite-plugin-glsl";
+import { templateCompilerOptions } from "@tresjs/core";
+import { fileURLToPath, URL } from "node:url";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -11,16 +11,22 @@ export default defineConfig({
     }),
     glsl(),
   ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
   build: {
     rollupOptions: {
       output: {
         manualChunks: {
-          three: ['three'],
+          three: ["three"],
         },
       },
     },
   },
   optimizeDeps: {
-    exclude: ['vue', 'three'],
+    exclude: ["vue", "three"],
   },
-})
+});
+
